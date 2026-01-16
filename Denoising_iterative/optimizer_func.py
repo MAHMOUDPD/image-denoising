dfb46daa0850943c7fba0492499b774975a493f0
mahmoudpd <60033244+MAHMOUDPD@users.noreply.github.com>
2026-01-16 10:38:41 +0700

    Update optimizer_func.py

--- Denoising_iterative/optimizer_func.py
+++ Denoising_iterative/optimizer_func.py
@@ -1,3 +1,9 @@
+'''
+Written by: Mahmoud M. Yahaya
+Contact address: mahmoudpd@gmail.com
+Date of last update: 19/08/2024
+'''
+
 import numpy as np
 from scipy.ndimage import median_filter
 from skimage import io, color, util
